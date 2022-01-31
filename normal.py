c98bb6ac4eb418bedfa4910923202d281eab3835
Abdul Rahman Ajeena <96892118+AbdulRahmanAjeena@users.noreply.github.com>
2022-01-30 19:34:13 -0500

    Create normal.py

---
+++ normal.py
@@ -0,0 +1,7 @@
+import random
+
+jpg = random.randint(1,50)
+
+print(f"https://api.beapdev.net/api/cats/{jpg}.jpg") # CATS
+print(f"https://api.beapdev.net/api/dogs/{jpg}.jpg") # DOGS
+print(f"https://api.beapdev.net/api/tigers/{jpg}.jpg") # TIGERS
